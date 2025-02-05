06e3fbc51c3b239536dd6457f3dca34a7d27964b
victortiang <changsheng.tiang@hotmail.com>
2025-02-05 22:13:36 +0800

    feat: add method in DataProcessor to upload data to Databricks

--- src/hotel_reservations/data_processor.py
+++ src/hotel_reservations/data_processor.py
@@ -1,6 +1,8 @@
 """Data processing module."""
 
 import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, to_utc_timestamp
 from sklearn.model_selection import train_test_split
 from hotel_reservations.config import ProjectConfig
 
@@ -8,9 +10,10 @@ from hotel_reservations.config import ProjectConfig
 class DataProcessor:
     """Data processing class."""
 
-    def __init__(self, pandas_df: pd.DataFrame, config: ProjectConfig):
+    def __init__(self, pandas_df: pd.DataFrame, config: ProjectConfig, spark: SparkSession):
         self.df = pandas_df
         self.config = config
+        self.spark = spark
 
     def preprocess(self):
         """Preprocess the data."""
@@ -26,6 +29,22 @@ class DataProcessor:
     def split_data(self, test_size: int = 0.2, random_state=0, **kwargs):
         """Split the data into train and test sets."""
 
-        x = self.df.drop(self.config.target, axis=1)
-        y = self.df[self.config.target]
-        return train_test_split(x, y, test_size=test_size, random_state=random_state, **kwargs)
+        return train_test_split(self.df, test_size=test_size, random_state=random_state, **kwargs)
+
+    def save_to_catalog(self, table_config: dict[str, pd.DataFrame]):
+        """Save df into Databricks tables.
+
+        Args:
+            table_config (dict[str, pd.DataFrame]): Set of dataframes to be pushed to \
+                Databricks catalog, where the key is the table name and value is the \
+                dataframe.
+        """
+
+        for table_name, df in table_config.items():
+            df_with_timestamp = self.spark.createDataFrame(df).withColumn(
+                "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+            )
+
+            df_with_timestamp.write.mode("append").saveAsTable(
+                f"{self.config.catalog_name}.{self.config.schema_name}.{table_name}"
+            )
