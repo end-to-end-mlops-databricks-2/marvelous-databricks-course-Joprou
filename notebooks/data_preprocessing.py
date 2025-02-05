ee2d46d907c2fb9721064c59804a0c726534edd3
victortiang <changsheng.tiang@hotmail.com>
2025-02-05 22:14:40 +0800

    chore: update Databricks config and add more steps in processor notebook

--- notebooks/data_preprocessing.py
+++ notebooks/data_preprocessing.py
@@ -8,10 +8,14 @@ import logging
 
 from hotel_reservations.config import ProjectConfig
 from hotel_reservations.data_processor import DataProcessor
+from pyspark.sql import SparkSession
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+spark = SparkSession.builder.getOrCreate()
 
 # COMMAND ----------
 
@@ -23,11 +27,14 @@ logging.info("Config loaded successfully")
 
 # COMMAND ----------
 
-data_processor = DataProcessor(pandas_df=data, config=config)
+data_processor = DataProcessor(pandas_df=data, config=config, spark=spark)
 
 # preprocess data
 data_processor.preprocess()
 # COMMAND ----------
 
-X_train, X_test, y_train, y_test = data_processor.split_data()
+train_set, test_set = data_processor.split_data()
+
+# COMMAND ----------
+data_processor.save_to_catalog({"train_set":train_set, "test_set":test_set})
 # COMMAND ----------
