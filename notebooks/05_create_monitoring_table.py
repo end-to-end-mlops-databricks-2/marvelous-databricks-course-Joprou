b4dfa2c599436650e05292c9f83946ae5f0ad1ae
victortiang <changsheng.tiang@hotmail.com>
2025-03-12 18:19:22 +0800

    feat(monitoring): add monitoring function, script and notebook

---
+++ notebooks/05_create_monitoring_table.py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+# %pip install --force-reinstall hotel_reservations-0.0.1-py3-none-any.whl
+# dbutils.library.restartPython()
+
+# COMMAND ----------
+
+
+# COMMAND ----------
+
+# %reload_ext autoreload"
+# %autoreload 2
+from databricks.sdk import WorkspaceClient
+from pyspark.sql import SparkSession
+from hotel_reservations.config import ProjectConfig
+from hotel_reservations.monitoring import create_or_refresh_monitoring_table
+
+spark = SparkSession.builder.config("spark.sql.session.timeZone", "UTC").getOrCreate()
+workspace = WorkspaceClient()
+config = ProjectConfig.from_yaml("../project_config.yml")
+CATALOG_SCHEMA = f"{config.catalog_name}.{config.schema_name}"
+
+# COMMAND ----------
+create_or_refresh_monitoring_table(
+    spark=spark,
+    workspace=workspace,
+    catalog_schema=CATALOG_SCHEMA,
+    inf_table_name=f"{CATALOG_SCHEMA}.hotel_reservations_payload",
+    monitoring_table_name=f"{CATALOG_SCHEMA}.hotel_reservations_monitoring",
+)
+
+# COMMAND ----------
---
+++ scripts/04_refresh_monitoring.py
@@ -0,0 +1,28 @@
+import argparse
+from databricks.sdk import WorkspaceClient
+from loguru import logger
+from pyspark.sql import SparkSession
+from hotel_reservations.config import ProjectConfig
+from hotel_reservations.monitoring import create_or_refresh_monitoring_table
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--root_path", action="store", default=None, type=str, required=True)
+parser.add_argument("--env", action="store", default=None, type=str, required=True)
+args = parser.parse_args()
+
+config = ProjectConfig.from_yaml(
+    config_path=f"{args.root_path}/files/project_config.yml", env=args.env
+)
+logger.info("Config loaded successfully.")
+CATALOG_SCHEMA = f"{config.catalog_name}.{config.schema_name}"
+
+spark = SparkSession.builder.config("spark.sql.session.timeZone", "UTC").getOrCreate()
+workspace = WorkspaceClient()
+
+create_or_refresh_monitoring_table(
+    spark=spark,
+    workspace=workspace,
+    catalog_schema=CATALOG_SCHEMA,
+    inf_table_name=f"{CATALOG_SCHEMA}.hotel_reservations_payload",
+    monitoring_table_name=f"{CATALOG_SCHEMA}.hotel_reservations_monitoring",
+)
